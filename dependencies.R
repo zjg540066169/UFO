119d42db25c17cb75a31158be8e4097dab2cda3a
zjg540066169 <jungang.zou@gmail.com>
2019-11-04 19:02:10 -0500

    dependencies update

--- dependencies.R
+++ dependencies.R
@@ -2,3 +2,13 @@ require(devtools)
 devtools::install_github("tidymodels/tidymodels")
 devtools::install_github('thomasp85/tidygraph')
 #devtools::install_github("r-spatial/sf")
+
+
+
+# load library
+library(tidyverse)
+library(viridis)
+library(ggridges)
+library(patchwork)
+library(rvest)
+library(tidygraph)
