066af427c593afb3b81d57d542639adb1ab8bb26
zjg540066169 <jungang.zou@gmail.com>
2019-12-03 03:06:02 -0500

    dependencies update

--- dependencies.R
+++ dependencies.R
@@ -1,7 +1,6 @@
 # download the library if they are not installed
 require(devtools)
 devtools::install_github("tidymodels/tidymodels", quiet = TRUE)
-devtools::install_github('thomasp85/tidygraph', quiet = TRUE)
 devtools::install_cran("ggraph", quiet = TRUE)
 devtools::install_cran("tidytext", quiet = TRUE)
 devtools::install_github("michaeldorman/mapsapi", quiet = TRUE)
@@ -14,6 +13,11 @@ devtools::install_cran("rgl", quiet = TRUE)
 devtools::install_cran("FactoMineR", quiet = TRUE)
 devtools::install_github("vqv/ggbiplot", quiet = TRUE)
 devtools::install_cran("mapview", quiet = TRUE)
+devtools::install_cran("plspm", quiet = TRUE)
+devtools::install_cran("polycor", quiet = TRUE)
+devtools::install_cran("misc3d", quiet = TRUE)
+
+
 
 # load library
 library(tidyverse)
